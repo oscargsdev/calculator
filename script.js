1d5621cf7735fcc11348150b9b3a447574c3874d
Oscar Garcia <oscare_gs@hotmail.com>
2022-08-10 20:43:16 -0500

    Functionality for plus operand button, chaining operations

--- script.js
+++ script.js
@@ -14,51 +14,110 @@ const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
 const zero = document.getElementById("zero");
 
+const plus = document.getElementById("plus");
+
 let displayNumber = "";
 
+let firstNumber = 0;
+let secondNumber = 0;
+let result = undefined;
+let operand = "";
+
 one.addEventListener('click', () => {
-    updateDisplay('1');
+    updateDisplayNumberButton('1');
 });
 
 two.addEventListener('click', () => {
-    updateDisplay('2');
+    updateDisplayNumberButton('2');
 });
 
 three.addEventListener('click', () => {
-    updateDisplay('3');
+    updateDisplayNumberButton('3');
 });
 
 four.addEventListener('click', () => {
-    updateDisplay('4');
+    updateDisplayNumberButton('4');
 });
 
 five.addEventListener('click', () => {
-    updateDisplay('5');
+    updateDisplayNumberButton('5');
 });
 
 six.addEventListener('click', () => {
-    updateDisplay('6');
+    updateDisplayNumberButton('6');
 });
 
 seven.addEventListener('click', () => {
-    updateDisplay('7');
+    updateDisplayNumberButton('7');
 });
 
 eight.addEventListener('click', () => {
-    updateDisplay('8');
+    updateDisplayNumberButton('8');
 });
 
 nine.addEventListener('click', () => {
-    updateDisplay('9');
+    updateDisplayNumberButton('9');
 });
 
 zero.addEventListener('click', () => {
-    updateDisplay('0');
+    updateDisplayNumberButton('0');
 });
 
-function updateDisplay(num){
+
+plus.addEventListener('click', () =>{
+    operatorButton("+");
+})
+
+function clearAllValues(){
+    firstNumber = 0;
+    secondNumber = 0;
+    displayNumber = 0;
+    operand = "";
+    result = "";
+
+    display.textContent = displayNumber;
+}
+
+function updateDisplayNumberButton(num){
     displayNumber = displayNumber + num;
     display.textContent = displayNumber;
+}
+
+function operate(firstNumber, secondNumber, operand){
+    switch (operand){
+        case "+":
+            return firstNumber + secondNumber;
+            
+        case "-":
+            return firstNumber - secondNumber;
+        case "*":
+            return firstNumber * secondNumber;
+        case "/":
+            return firstNumber / secondNumber;
+    }
+}
+
+
+function operatorButton(op){
+    if (operand == ""){
+        firstNumber = parseInt(displayNumber);
+        operand = op;
+        displayNumber = '';
+        // display.textContent = displayNumber;
+    }
+    else{
+        
+        secondNumber = parseInt(displayNumber);
+        result = operate(firstNumber, secondNumber, operand);
+        firstNumber = result;
+        
+        display.textContent = firstNumber;
+        displayNumber = "";
+        operand = op;
+    
+        
+        
+    }    
 }
 
 
